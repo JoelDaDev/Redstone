c625d20f8cdc83ae63ef5f677fce0755f410ce90
JoelDaDev <joelharpin09@gmail.com>
2026-01-30 15:55:05 -0600

    Create Project

--- src/core/project_manager.py
+++ src/core/project_manager.py
@@ -1,5 +1,72 @@
+import requests
+from pathlib import Path
+import os
+import datetime
+import json
+from utils.const import *
+
 class ProjectManager:
-    def __init__(self, ui):
+    def __init__(self, ui, mainDirectory):
         super().__init__()
         self.ui = ui
+        self.mainDirectory = mainDirectory
         
+        self.elements = {}
+        self.apiURL = "https://joelalexander.dev/RedstoneAPI/api/"
+    
+    def newProject(self, packDetails):
+        self.packDetails = packDetails
+        
+        self.installVersion(packDetails["version"])
+
+        self.saveProject()
+    
+    def saveProject(self):
+        self.projectDirectory = self.mainDirectory / 'workspaces' / self.packDetails["namespace"]
+        os.makedirs(self.projectDirectory, exist_ok=True)
+
+        with open(self.projectDirectory / 'project.json', 'w') as file:
+            data = {
+                "app_version": APP_VERSION,
+                "metadata": {
+                    "last_edited": datetime.datetime.now(datetime.timezone.utc).isoformat()
+                },
+                "packDetails": self.packDetails
+            }
+            json.dump(data, file, indent=4)
+        
+        with open(self.projectDirectory / 'elements.json', 'w') as file:
+            json.dump(self.elements, file, indent=4)
+
+        os.makedirs(self.projectDirectory / 'assets', exist_ok=True)
+
+        manifestPath = self.mainDirectory / 'workspaces' / 'manifest.json'
+
+        # If it exists, load Manifest
+        if os.path.exists(manifestPath):
+            with open(manifestPath, 'r') as f:
+                manifest = json.load(f)
+        else:
+            manifest = {"workspaces": []}
+        
+        # If not listed, add current workspace
+        namespace = self.packDetails["namespace"]
+        if namespace not in manifest["workspaces"]:
+            manifest["workspaces"].append(namespace)
+            with open(manifestPath, 'w') as f:
+                json.dump(manifest, f, indent=4)
+
+    def installVersion(self, version):
+        pass
+
+    def getVersions(self):
+        versionsUrl = self.apiURL + "versions.json"
+        response = requests.get(versionsUrl)
+        response.raise_for_status()
+
+        return response.json()
+
+if __name__ == '__main__':
+    projMan = ProjectManager("")
+    versions = projMan.getVersions()
+    print(versions)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from PySide6.QtWidgets import QMainWindow, QApplication, QDialog
 import sys
+from pathlib import Path
 from ui.ui_mainWindow import Ui_MainWindow
 from ui.ui_newProject import Ui_Dialog as newProj
+from core.project_manager import ProjectManager
 
 class App(QMainWindow):
     def __init__(self):
@@ -11,14 +13,43 @@ class App(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        if getattr(sys, 'frozen', False):
+            # Binary mode
+            self.mainDirectory = Path(sys._MEIPASS)
+        else:
+            # Dev mode
+            self.mainDirectory = Path(__file__).resolve().parent.parent
+
+        self.projectManager = ProjectManager(self.ui, self.mainDirectory)
+
         self.ui.newProjectBtn.clicked.connect(self.openNewProjectDialog)
     
     def openNewProjectDialog(self):
         dialog = QDialog(self)
         dialogUI = newProj()
         dialogUI.setupUi(dialog)
-        dialog.exec()
 
+        # Pulls versions & adds them to UI
+        versionList = self.projectManager.getVersions()
+        for version in versionList["versions"]:
+            dialogUI.packVersion.addItem(version)
+        
+        dialogUI.createProjectBtn.clicked.connect(lambda: self.newProject(dialog, dialogUI))
+
+        dialog.exec()
+    
+    def newProject(self, dialog, dialogUI):
+        packDetails = {
+            "name": dialogUI.packTitle.text(),
+            "namespace": dialogUI.packNamespace.text(),
+            "description": dialogUI.packDescription.text(),
+            "version": dialogUI.packVersion.currentText()
+        }
+        self.projectManager.newProject(packDetails)
+
+        dialog.accept()
+
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = App()
